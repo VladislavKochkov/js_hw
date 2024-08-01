04659429205e89b7bf778d2dfbe42071bcc7124c
Vladislav <kochkov.brd@gmail.com>
2024-08-01 23:20:04 +0300

    Created hw

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+//task #1
+let admin;
+let name;
+name='John';
+admin = name;
+alert(admin);
+
+
+//task #2
+let planetName = "Земля";
+let visitorName = "Имя поситителя";
+
+//task #3
+let result;
+result = '10' + 10;
+let typeofResult = typeof result;
+console.log(typeofResult);
