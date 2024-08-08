f77eeabd4887683e5b3dcbbc60b1b97e0bbbe9f8
Vladislav <kochkov.brd@gmail.com>
2024-08-09 00:34:51 +0300

    Created hw

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const number1 = parseFloat(prompt("Введите число:"));
+
+const number2 = parseFloat(prompt("Введите число:"));
+
+const number3 = parseFloat(prompt("Введите число:"));
+
+const average = (number1 + number2 + number3) / 3;
+
+alert("Среднее значение: " + average);
