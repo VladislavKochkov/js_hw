a9d533b09f0d452b53427c6402c08e4930d7c424
Vladislav <kochkov.brd@gmail.com>
2024-08-12 23:44:30 +0300

    Created hw

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+
+const globals = require('globals');
+
+module.exports = [
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        rules: {
+            'no-console': 'warn',
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'eqeqeq': 'error',
+            'no-unused-vars': 'warn',
+            'no-var': 'error',
+            'prefer-const': 'error',
+        },
+    },
+];
