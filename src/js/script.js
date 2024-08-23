21ca9c29cdbe3a1a37340c1d5d52f673762900ee
Vladislav <kochkov.brd@gmail.com>
2024-08-24 02:31:43 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
-'use strict'
+'use strict';
+
+function padString(str, length, char = ' ', padLeft = false) {
+    if (
+        typeof str !== 'string' ||
+        typeof length !== 'number' ||
+        typeof char !== 'string' ||
+        typeof padLeft !== 'boolean'
+    ) {
+        return 'some error: invalid type of argument';
+    }
+    if (char.length !== 1) {
+        return 'some error: char must be a single character';
+    }
+    if (length < 0) {
+        return 'some error: length must be a non-negative number';
+    }
+    if (str.length > length) {
+        return str.substr(0, length);
+    }
+
+    const paddingLength = length - str.length;
+    const padding = char.repeat(paddingLength);
+
+    if (padLeft) {
+        return padding + str;
+    }
+    return str + padding;
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', true));
+console.log(padString('hello', 2));
