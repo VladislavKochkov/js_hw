3a43d4826416e92c527d6751ee3a6f06538a07f8
Vladislav <kochkov.brd@gmail.com>
2024-09-07 23:40:40 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-'use strict'
+'use strict';
+
+const yourNumbers = () => {
+    const length = +prompt('введите количество элементов вашего массива');
+    const arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(+prompt('введите количество элементов вашего массива'));
+    }
+    console.log(arr);
+    return arr;
+};
+const yourArr = yourNumbers();
+
+const increasing = (arr) => {
+    arr.sort((a, b) => a - b);
+    console.log(arr);
+    return arr;
+};
+const newArr = increasing(yourArr);
+
+const deleteNumbers = (arr) => {
+    arr.splice(1, 3);
+    console.log(arr);
+    return arr;
+};
+deleteNumbers(newArr);
