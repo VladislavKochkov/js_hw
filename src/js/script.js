d381aebd17600676ea403598ca171e1da59019a9
Vladislav <kochkov.brd@gmail.com>
2024-09-19 20:26:30 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-'use strict'
+'use strict';
+
+//Task #1
+
+function getFactorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * getFactorial(n - 1);
+}
+console.log(getFactorial(5));
+
+//Task #2
+
+function pow(num, degree) {
+    if (degree === 0) {
+        return 1;
+    }
+    return num * pow(num, degree - 1);
+}
+console.log(pow(5, 3));
+
+//Task #3
+
+function recursiveSum(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    return recursiveSum(a + 1, b - 1);
+}
+console.log(recursiveSum(10, 23));
