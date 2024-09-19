6dfd06ee69240b82f5ce776ab4ccfe802a2026dd
Vladislav <kochkov.brd@gmail.com>
2024-09-19 19:51:51 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+function customBind(fn, context, ...args) {
+    return function (...newArgs) {
+        const combinedArgs = args.concat(newArgs);
+        const symbolKey = Symbol();
+        context[symbolKey] = fn;
+        const result = context[symbolKey](...combinedArgs);
+        delete context[symbolKey];
+        return result;
+    };
+}
+const FirstName = {
+    name: 'Vladislav',
+};
+function printX(extraText) {
+    console.log(`${extraText}: ${this.name}`);
+}
+const boundFunc = customBind(printX, FirstName, 'name');
+boundFunc();
