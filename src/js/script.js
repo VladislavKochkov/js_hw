b222fd47ddfea818659869c3ff83b25dee28ef8f
Vladislav <kochkov.brd@gmail.com>
2024-09-24 18:53:57 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+    },
+};
+// ladder.up()
+// ladder.up()
+// ladder.down()
+// ladder.showStep() // 1
+ladder.up().up().down().showStep(); // 1
