e3c53b1c574f57685efe56f63f6b2c1229b28b87
Vladislav <kochkov.brd@gmail.com>
2024-09-24 18:52:09 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const ourSum = () => {
+    let result = 0;
+    return (number) => {
+        result = result + number;
+        return result;
+    };
+};
+const sum = ourSum();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+console.log(sum(50));
+console.log(sum(100));
+console.log(sum(1000));
