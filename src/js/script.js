563f597eea7a43937e529f93b1da71b97dbc8aba
Vladislav <kochkov.brd@gmail.com>
2024-09-27 23:32:35 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Масив з назвами зображень
+const img = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+];
+const nineImg = () => {
+    document.getElementById('img').src =
+        `images/${img[~~(Math.random() * img.length)]}`;
+};
+nineImg();
