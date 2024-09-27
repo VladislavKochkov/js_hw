a9976be9d461d3e16326fd514794854cc7c7c3af
Vladislav <kochkov.brd@gmail.com>
2024-09-27 17:33:12 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const createTable = document.getElementById('generateTable');
+let counter = 1;
+for (let i = 0; i < 10; i++) {
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < 10; j++) {
+        const cell = document.createElement('td');
+        cell.textContent = counter;
+
+        counter++;
+
+        row.appendChild(cell);
+    }
+    createTable.appendChild(row);
+}
