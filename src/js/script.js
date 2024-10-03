04dfbb60ab47ed371b9368880d9cb24f1a77c83c
Vladislav <kochkov.brd@gmail.com>
2024-10-03 17:47:43 +0300

    corrected hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,40 @@
 'use strict';
 
-// Масив з назвами зображень
-const img = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-];
-const nineImg = () => {
-    document.getElementById('img').src =
-        `images/${img[~~(Math.random() * img.length)]}`;
-};
-nineImg();
+//  function to select and display a random image
+function showRandomImage() {
+    const images = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg',
+    ];
+    const randomImage = getRandomElement(images);
+    displayImage(`./images/${randomImage}`);
+}
+
+// function to get a random element from an array
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+// function to create an HTML image element
+function createImageElement(src, alt = 'Random Image') {
+    const imgElement = document.createElement('img');
+    imgElement.src = src;
+    imgElement.alt = alt;
+    return imgElement;
+}
+
+// function to display the image on the page
+function displayImage(imagePath) {
+    const imgElement = createImageElement(imagePath);
+    document.body.appendChild(imgElement);
+}
+
+showRandomImage();
