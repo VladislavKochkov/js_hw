09a675fa1990b3022e4a3737fe4120ea5e514b1c
Vladislav <kochkov.brd@gmail.com>
2024-10-03 23:09:15 +0300

    created hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const inputText = document.getElementById('textField');
+const displayDiv = document.getElementById('appear');
+displayDiv.style.display = 'none';
+document.addEventListener('focusin', () => {
+    if (document.activeElement === inputText) {
+        displayDiv.style.display = 'flex';
+    }
+});
+document.addEventListener('focusout', () => {
+    if (document.activeElement !== inputText) {
+        displayDiv.style.display = 'none';
+    }
+});
