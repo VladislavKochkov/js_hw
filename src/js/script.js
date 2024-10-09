7d90c9dcfafca4a2037bc2c4053e02ecc63ffbc5
Vladislav <kochkov.brd@gmail.com>
2024-10-09 14:43:39 +0300

    corrected hw

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+(function () {
+    const generateList = (array) => {
+        const ul = document.createElement('ul');
+        array.forEach((item) => {
+            const li = document.createElement('li');
+            li.textContent = item;
+            if (Array.isArray(item)) {
+                li.append(generateList(item));
+            }
+            ul.append(li);
+        });
+        return ul;
+    };
+
+    const arrOfNum = [1, 2, 3];
+    document.body.append(generateList(arrOfNum));
+
+    const arrOfArrays = [1, 2, [1.1, 1.2, 1.3], 3];
+    document.body.appendChild(generateList(arrOfArrays));
+})();
